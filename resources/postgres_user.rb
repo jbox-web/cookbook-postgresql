4f62eff0e23472b58a25af3fa2c35f7e2d8805fe
Nicolas Rodriguez <nicoladmin@free.fr>
2019-01-27 07:22:06 +0100

    Make User rights parameterizable

--- resources/postgres_user.rb
+++ resources/postgres_user.rb
@@ -2,10 +2,11 @@ resource_name :postgres_user
 provides :postgres_user
 
 property :user,       String, name_property: true
+property :rights,     String, default: '-SDRw'
 property :admin_user, String, default: 'postgres'
 
 action :install do
-  execute "createuser -U #{new_resource.admin_user} -SDRw #{new_resource.user}" do
+  execute "createuser -U #{new_resource.admin_user} #{new_resource.rights} #{new_resource.user}" do
     only_if { !ChefPostgresqlCookbook::Helper.user_exists?(new_resource.user) }
   end
 end
